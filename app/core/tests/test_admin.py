63722cbe3af65fb9446292331f975d2368411941
shangar-t-a <shangar202000@gmail.com>
2023-07-02 10:07:09 +0530

    feat: django admin create user page
    Signed-off-by: shangar-t-a <shangar202000@gmail.com>

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -23,7 +23,7 @@ class AdminSiteTests(TestCase):
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='user@email.com',
+            email='user@example.com',
             password='testpass123',
             name='Test User',
         )
@@ -44,6 +44,16 @@ class AdminSiteTests(TestCase):
         """
 
         url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """
+        Test the create user page works
+        """
+
+        url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
