922d0e9ca7ee95839329f4ddd7510d5bb7b87839
Keys (Shangar) <shangar202000@gmail.com>
2023-09-12 16:05:16 +0530

    feat: add poetry, user app and swagger
    Signed-off-by: Keys (Shangar) <shangar202000@gmail.com>

---
+++ app/main.py
@@ -0,0 +1,34 @@
+"""
+Recipe App launch file
+"""
+
+import os
+import sys
+
+
+def main() -> None:
+    """
+    Main call for Recipe App
+    """
+
+    os.environ["DB_HOST"] = "localhost"
+    os.environ["DB_NAME"] = "recipe_app"
+    os.environ["DB_USER"] = "shangar"
+    os.environ["DB_PASS"] = "temp1234"
+
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
