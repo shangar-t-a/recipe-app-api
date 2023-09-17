0d2ddb3557f6a436196336691bb6d60631d31c56
Keys (Shangar) <shangar202000@gmail.com>
2023-09-17 15:49:10 +0530

    style: auto code formatting
    Signed-off-by: Keys (Shangar) <shangar202000@gmail.com>

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-cn78rkn#)tgib7f5yo7zxy1mhr7)4eafq)=l08zdnhupv8)l11"
+SECRET_KEY = (
+    "django-insecure-cn78rkn#)tgib7f5yo7zxy1mhr7)4eafq)=l08zdnhupv8)l11"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
