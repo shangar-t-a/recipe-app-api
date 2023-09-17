133d0e9fd4bae39a5fd922c0a03366b89e5772ef
Keys (Shangar) <shangar202000@gmail.com>
2023-09-17 23:16:26 +0530

    docs: minor docs change
    Signed-off-by: Keys (Shangar) <shangar202000@gmail.com>

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -37,7 +37,7 @@ from recipe import serializers
             OpenApiParameter(
                 "ingredients",
                 OpenApiTypes.STR,
-                description="Comma separated list of ingredients IDs to filter",
+                description="Comma separated list of ingredient IDs to filter",
             ),
         ]
     )
