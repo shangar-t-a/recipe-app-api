c026d361edff91ec6eeeaed8846c8d14e0ed6904
Keys (Shangar) <shangar202000@gmail.com>
2023-09-17 22:04:44 +0530

    feat: add image API
    Signed-off-by: Keys (Shangar) <shangar202000@gmail.com>

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,8 @@
 """
 Test for models
 """
+import os
+from unittest.mock import patch
 from decimal import Decimal
 
 from django.test import TestCase
@@ -103,3 +105,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch("core.models.uuid.uuid4")
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path."""
+        uuid = "test-uuid"
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, "example.jpg")
+
+        self.assertEqual(
+            file_path, os.path.join("uploads", "recipe", f"{uuid}.jpg")
+        )
